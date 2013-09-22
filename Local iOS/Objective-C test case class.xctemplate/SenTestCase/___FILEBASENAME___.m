4408eda23db707add57a5e67e252c9513914298b
Graham Lee <graham@bignerdranch.com>
2013-09-22 17:02:25 +0100

    Remove the fail.

---
+++ Local iOS/Objective-C test case class.xctemplate/SenTestCase/___FILEBASENAME___.m
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_testSubclass___
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+@end
---
+++ Local iOS/Objective-C test case class.xctemplate/XCTestCase/___FILEBASENAME___.m
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_testSubclass___
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+@end
