5780d5e4c32e20ff365bd3eb5b7033b804d8e98b
Graham Lee <graham@bignerdranch.com>
2013-09-22 17:00:27 +0100

    Just sayin'.

--- Local iOS/Objective-C class.xctemplate/UIViewControlleriPad/___FILEBASENAME___.m
+++ Local iOS/Objective-C class.xctemplate/UIViewControlleriPad/___FILEBASENAME___.m
@@ -14,25 +14,4 @@
 
 @implementation ___FILEBASENAMEASIDENTIFIER___
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
