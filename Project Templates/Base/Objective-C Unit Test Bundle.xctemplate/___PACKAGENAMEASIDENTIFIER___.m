f3f7a7111a94b82cbcd07742ad7305eebf2c0ec3
Graham Lee <graham@bignerdranch.com>
2013-09-23 08:42:26 +0100

    Remove the fail from the base unit testing template.

---
+++ Project Templates/Base/Objective-C Unit Test Bundle.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <___UNIT_TEST_FRAMEWORK_NAME___/___UNIT_TEST_FRAMEWORK_NAME___.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___UNIT_TEST_BASE_CLASS___
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+@end
